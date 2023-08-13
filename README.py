fbd1a302c0a11161fb0fbfb717f2c528308d5010
mar1o0o0o <142157551+mar1o0o0o@users.noreply.github.com>
2023-08-13 15:00:40 +0200

    Create README.py

---
+++ README.py
@@ -0,0 +1,4 @@
+print("Hello world!")
+print("I'm Mar1oo")
+current_language = "Python"
+print("I'm currently self learning " + current_language + ", but my goal is to learn more programming languages!")
